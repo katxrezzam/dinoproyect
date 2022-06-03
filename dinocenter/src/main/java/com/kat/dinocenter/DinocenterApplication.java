7e73e79be0d5ff497bd208040dab4e92be9658d4
katxrezzam <katxrezzam@hotmail.com>
2022-06-03 05:03:57 -0500

    version 1.0

---
+++ dinocenter/src/main/java/com/kat/dinocenter/DinocenterApplication.java
@@ -0,0 +1,13 @@
+package com.kat.dinocenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinocenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinocenterApplication.class, args);
+    }
+
+}
---
+++ dinocenter/src/test/java/com/kat/dinocenter/DinocenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kat.dinocenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinocenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
